7bf8dfc53bbfecdb0753b675d72a139563b62de0
zazaborisovi <geozedx123@mail.ru>
2024-09-18 21:19:23 +0400

    0.1 added register/login/logout

---
+++ script.js
@@ -0,0 +1,13 @@
+class User{
+    constructor(name , password){
+        this.name = name
+        this.password = password
+    }
+}
+class Message {
+    constructor(sender, receiver, content) {
+        this.sender = sender
+        this.receiver = receiver
+        this.content = content
+    }
+}
